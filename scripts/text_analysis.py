47d6dca76cdb5c43abb570dd6538a89c075f2e56
Yayerad <yayemekonnen@gmail.com>
2024-08-31 18:31:52 +0300

    added visualization

--- scripts/text_analysis.py
+++ scripts/text_analysis.py
@@ -1,16 +1,25 @@
-# scripts/text_analysis.py
-
 from textblob import TextBlob
 
 def sentiment_analysis(df):
     """
-    Perform sentiment analysis on the headlines.
+    Perform sentiment analysis on the headlines and categorize the sentiment.
 
     Parameters:
     - df (pd.DataFrame): DataFrame containing the dataset.
 
     Returns:
-    - df (pd.DataFrame): DataFrame with an additional column for sentiment polarity.
+    - df (pd.DataFrame): DataFrame with additional columns for sentiment polarity and sentiment category.
     """
     df['sentiment'] = df['headline'].apply(lambda x: TextBlob(x).sentiment.polarity)
+    
+    # Categorize sentiment
+    def categorize_sentiment(score):
+        if score > 0:
+            return 'positive'
+        elif score == 0:
+            return 'neutral'
+        else:
+            return 'negative'
+
+    df['sentiment_category'] = df['sentiment'].apply(categorize_sentiment)
     return df
